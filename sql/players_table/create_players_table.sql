790ed35a1d9134c09fb57902633fb6c536937eed
mariuszjanuszko <mtjanuszko@gmail.com>
2025-07-19 19:38:42 +0200

    Update create_players_table.sql

--- sql/players_table/create_players_table.sql
+++ sql/players_table/create_players_table.sql
@@ -1,3 +1,13 @@
-SELECT first_name name, last_name surname, birth_date
-FROM players
-WHERE UPPER(nationality) = 'POLAND'
+CREATE TABLE players (
+    player_id NUMBER PRIMARY KEY,
+    player_number NUMBER(2) UNIQUE,
+    position VARCHAR2(50),
+    first_name VARCHAR2(50),
+    last_name VARCHAR2(50),
+    birth_date DATE,
+    nationality VARCHAR2(50),
+    height_cm NUMBER(3),
+    preferred_foot VARCHAR2(10) CHECK (preferred_foot IN ('left','right','both')),
+    joined_club DATE,
+    contract_until DATE
+);
