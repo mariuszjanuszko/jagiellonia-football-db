b5219912a4e726af1f97159406bda5483138435c
mariuszjanuszko <mtjanuszko@gmail.com>
2025-09-19 19:11:39 +0200

    Update select_transferred_players_detail_by_typing_other_club_name.sql

--- sql/queries/select_transferred_players_detail_by_typing_other_club_name.sql
+++ sql/queries/select_transferred_players_detail_by_typing_other_club_name.sql
@@ -2,7 +2,8 @@ SELECT
 		t.player_name,
 		t.transfer_type,
 		t.transfer_direction,
-		t.transfer_fee_eur
+		t.transfer_fee_eur,
+		c.club_name AS "FROM/TO_CLUB" 
 FROM TRANSFERS t
 JOIN CLUBS c ON t.OTHER_CLUB_ID = c.CLUB_ID
 WHERE lower(c.CLUB_NAME) like lower('%' ||  :club  || '%')
